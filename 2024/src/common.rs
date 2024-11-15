0b1f8c5666362a5cc4aafa0e57fff20a9602cd46
Gabriel White-Vega <whitevegagabriel@gmail.com>
2024-11-15 15:34:33 -0500

    Set up skeleton for aoc 2024
    I'm using a test-based approach instead of binary-based approach this
    year

---
+++ 2024/src/common.rs
@@ -0,0 +1,24 @@
+use lazy_static::lazy_static;
+use std::{cmp::Eq, env, fmt::Debug, fs::read_to_string, path::PathBuf, time};
+
+lazy_static! {
+    pub(crate) static ref MANIFEST_DIR: String = env::var("CARGO_MANIFEST_DIR").unwrap();
+}
+
+#[allow(unused)]
+pub(crate) fn test<T: Debug + Eq, F: Fn(&str) -> T>(input_file: &PathBuf, f: F, expected: T) {
+    test_with_params(input_file, |s: &str, _: ()| { f(s) }, (), expected);
+}
+
+#[allow(unused)]
+pub(crate) fn test_with_params<P, T: Debug + Eq, F: Fn(&str, P) -> T>(input_file: &PathBuf, f: F, params: P, expected: T) {
+    let start = time::Instant::now();
+    {
+        let input = read_to_string(input_file).unwrap();
+        let actual = f(&input, params);
+
+        assert_eq!(expected, actual);
+    }
+    let elapsed = start.elapsed();
+    println!("Elapsed: {} millis", elapsed.as_millis());
+}
