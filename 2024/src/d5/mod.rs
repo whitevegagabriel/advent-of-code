e4f96b3aa87cbc58d2d6f6db3bf020a741740aaa
Gabriel White-Vega <gwhitevega@google.com>
2024-12-05 10:55:18 -0500

    Solve day 5 part 2 of 2024

--- 2024/src/d5/mod.rs
+++ 2024/src/d5/mod.rs
@@ -1,6 +1,7 @@
 use crate::common::test;
 use itertools::Itertools;
 use std::collections::{HashMap, HashSet};
+use std::ops::AddAssign;
 
 const MODULE: &str = module_path!();
 
@@ -21,7 +22,7 @@ fn p2_example() {
 
 #[test]
 fn p2_input() {
-    test("input", MODULE, p2, 0);
+    test("input", MODULE, p2, 3062);
 }
 
 fn p1(input: &str) -> usize {
@@ -35,7 +36,7 @@ fn p1(input: &str) -> usize {
             let Some(rule) = rules.get(page) else {
                 continue;
             };
-            
+
             if rule.iter().any(|r| seen.contains(r)) {
                 return None;
             }
@@ -45,8 +46,67 @@ fn p1(input: &str) -> usize {
     }).sum()
 }
 
-fn p2(_input: &str) -> usize {
-    0
+fn p2(input: &str) -> usize {
+    let (rules, updates) = parse_input(input);
+    
+    let mut incorrect_updates = updates.into_iter().filter(|pages| {
+        let mut seen = HashSet::new();
+        for page in pages.iter() {
+            seen.insert(*page);
+
+            let Some(rule) = rules.get(page) else {
+                continue;
+            };
+
+            if rule.iter().any(|r| seen.contains(r)) {
+                return true;
+            }
+        }
+
+        false
+    }).collect_vec();
+    
+    for incorrect_update in incorrect_updates.iter_mut() {
+        let rankings = generate_rankings(&rules, incorrect_update);
+
+        incorrect_update.sort_by_key(|n| {
+            rankings.get(n).unwrap_or(&0)
+        })
+    }
+    
+    incorrect_updates.iter().map(|pages| pages[pages.len()/2]).sum()
+}
+
+fn generate_rankings(rules: &HashMap<usize, Vec<usize>>, values: &[usize]) -> HashMap<usize, usize> {
+    let mut new_rules = rules.clone();
+    let values: HashSet<_> = values.iter().collect();
+    
+    new_rules.retain(|k, _| {
+        values.contains(k)
+    });
+    
+    for rights in new_rules.values_mut() {
+        rights.retain(|v| values.contains(v));
+    }
+    
+    let mut rankings = HashMap::<usize, usize>::new();
+
+    for left in new_rules.keys() {
+        recursive_increment(left, &new_rules, &mut rankings);
+    }
+    
+    rankings
+}
+
+fn recursive_increment(left: &usize, rules: &HashMap<usize, Vec<usize>>, rankings: &mut HashMap<usize, usize>) {
+    let Some(rights) = rules.get(left) else {
+        return;
+    };
+    
+    for right in rights {
+        rankings.entry(*right).or_insert(0).add_assign(1);
+        recursive_increment(right, rules, rankings);
+    }
 }
 
 fn parse_input(input: &str) -> (HashMap<usize, Vec<usize>>, Vec<Vec<usize>>) {
