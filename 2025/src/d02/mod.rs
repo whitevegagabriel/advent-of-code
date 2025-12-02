2af57a61f4b05613813361b4a32cd598397b7be6
Gabriel White-Vega <whitevegagabriel@gmail.com>
2025-12-02 11:25:34 -0500

    2025 Day 2

--- 2025/src/d02/mod.rs
+++ 2025/src/d02/mod.rs
@@ -1,20 +1,21 @@
 use crate::common::test;
+use itertools::Itertools;
 
 const MODULE: &str = module_path!();
 
 #[test]
 fn p1_example() {
-    test("example", MODULE, p1, 0);
+    test("example", MODULE, p1, 1227775554);
 }
 
 #[test]
 fn p1_input() {
-    test("input", MODULE, p1, 0);
+    test("input", MODULE, p1, 53420042388);
 }
 
 #[test]
 fn p2_example() {
-    test("example", MODULE, p2, 0);
+    test("example", MODULE, p2, 4174379265);
 }
 
 #[test]
@@ -23,9 +24,40 @@ fn p2_input() {
 }
 
 fn p1(input: &str) -> usize {
-    0
+    solve(input, |candidate| {
+        let candidate_string = candidate.to_string();
+        let halfway = candidate_string.len() / 2;
+        candidate_string[0..halfway] == candidate_string[halfway..]
+    })
 }
 
 fn p2(input: &str) -> usize {
-    0
+    solve(input, |candidate| {
+        let candidate_string = candidate.to_string();
+        // try all possible valid pattern lengths. short-circuit on the first pattern to succeed
+        (1..=candidate_string.len() / 2).rev().any(|chunk_size| {
+            candidate_string
+                .chars()
+                .chunks(chunk_size)
+                .into_iter()
+                .map(|chunk| chunk.collect_vec())
+                .all_equal()
+        })
+    })
+}
+
+fn solve(input: &str, candidate_filter: fn(&usize) -> bool) -> usize {
+    input
+        .split(',')
+        .flat_map(|range_str| {
+            let (start, end) = range_str
+                .split('-')
+                .map(|num| num.parse::<usize>().unwrap())
+                .collect_tuple()
+                .unwrap();
+
+            start..=end
+        })
+        .filter(candidate_filter)
+        .sum()
 }
