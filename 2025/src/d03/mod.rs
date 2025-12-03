9798cf5e1afcba134ccb15766a29a3d030015c6c
Gabriel White-Vega <whitevegagabriel@gmail.com>
2025-12-03 11:05:55 -0500

    2025 Day 3 Part 2

--- 2025/src/d03/mod.rs
+++ 2025/src/d03/mod.rs
@@ -16,12 +16,12 @@ fn p1_input() {
 
 #[test]
 fn p2_example() {
-    test("example", MODULE, p2, 0);
+    test("example", MODULE, p2, 3121910778619);
 }
 
 #[test]
 fn p2_input() {
-    test("input", MODULE, p2, 0);
+    test("input", MODULE, p2, 175053592950232);
 }
 
 fn p1(input: &str) -> usize {
@@ -35,5 +35,29 @@ fn p1(input: &str) -> usize {
 }
 
 fn p2(input: &str) -> usize {
-    0
+    input
+        .lines()
+        .map(|line| {
+            let bank = line
+                .chars()
+                .map(|char| char.to_digit(10).unwrap() as usize)
+                .collect_vec();
+            max_combination(&bank, 12, 0)
+        })
+        .sum()
+}
+
+fn max_combination(digits: &[usize], take: usize, prev: usize) -> usize {
+    if take > digits.len() || take == 0 || digits.is_empty() {
+        return prev;
+    }
+
+    let max_value = digits[..=digits.len() - take].iter().max().unwrap();
+    digits
+        .iter()
+        .enumerate()
+        .filter_map(|(i, value)| if value == max_value { Some(i) } else { None })
+        .map(|max_index| max_combination(&digits[max_index + 1..], take - 1, prev * 10 + max_value))
+        .max()
+        .unwrap()
 }
