b153f449a776a8f567ba7e71097e70a991d98ef3
Gabriel White-Vega <whitevegagabriel@gmail.com>
2025-12-08 08:32:37 -0500

    2025 Day 8

--- 2025/src/d08/mod.rs
+++ 2025/src/d08/mod.rs
@@ -1,31 +1,82 @@
-use crate::common::test;
+use crate::common::{test, test_with_params};
+use disjoint::DisjointSet;
+use itertools::Itertools;
 
 const MODULE: &str = module_path!();
 
 #[test]
 fn p1_example() {
-    test("example", MODULE, p1, 0);
+    test_with_params("example", MODULE, p1, 10, 40);
 }
 
 #[test]
 fn p1_input() {
-    test("input", MODULE, p1, 0);
+    test_with_params("input", MODULE, p1, 1000, 80446);
 }
 
 #[test]
 fn p2_example() {
-    test("example", MODULE, p2, 0);
+    test("example", MODULE, p2, 25272);
 }
 
 #[test]
 fn p2_input() {
-    test("input", MODULE, p2, 0);
+    test("input", MODULE, p2, 51294528);
 }
 
-fn p1(input: &str) -> usize {
-    0
+fn p1(input: &str, k_closest: usize) -> usize {
+    let num_coords = input.lines().count();
+    let mut disjoint_set = DisjointSet::with_len(num_coords);
+    let mut num_processed = 0;
+    for (i1, _, i2, _) in parse_combinations_sorted_by_distance(input) {
+        disjoint_set.join(i1, i2);
+        num_processed += 1;
+        if num_processed == k_closest {
+            break;
+        }
+    }
+    disjoint_set
+        .sets()
+        .iter()
+        .map(|set| set.len())
+        .sorted()
+        .rev()
+        .take(3)
+        .product()
 }
 
 fn p2(input: &str) -> usize {
-    0
+    let num_coords = input.lines().count();
+    let mut disjoint_set = DisjointSet::with_len(num_coords);
+    for (i1, coord1, i2, coord2) in parse_combinations_sorted_by_distance(input) {
+        disjoint_set.join(i1, i2);
+        if disjoint_set.sets().len() == 1 {
+            return coord1.0 * coord2.0;
+        }
+    }
+    panic!("Should have joined the disjoint set");
+}
+
+type Point3 = (usize, usize, usize);
+
+fn parse_combinations_sorted_by_distance(input: &str) -> Vec<(usize, Point3, usize, Point3)> {
+    input
+        .lines()
+        .map(|line| {
+            line.split(',')
+                .map(|num_str| num_str.parse::<usize>().unwrap())
+                .collect_tuple::<(_, _, _)>()
+                .unwrap()
+        })
+        .enumerate()
+        .tuple_combinations()
+        .map(|((i1, coord1), (i2, coord2))| {
+            let square_distance = (coord1.0.abs_diff(coord2.0)).pow(2)
+                + (coord1.1.abs_diff(coord2.1)).pow(2)
+                + (coord1.2.abs_diff(coord2.2)).pow(2);
+            (square_distance, i1, coord1, i2, coord2)
+        })
+        .sorted()
+        .map(|(_, i1, coord1, i2, coord2)| (i1, coord1, i2, coord2))
+        .collect_vec()
 }
